aad526a2af6a04869f10117af6250dd14a429428
TabiaAfrose-git <tabiaafrose16@gmail.com>
2024-08-17 21:31:22 +0600

    first commit

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+    return (
+        <div>
+        <h3 className='text-4xl	font-bold'>knowledge cafe
+
+
+    </h3>
+        </div>
+    );
+};
+
+export default Header;
